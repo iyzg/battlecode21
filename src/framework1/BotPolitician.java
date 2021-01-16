ecdc23ec54c2589942b96a05c08e0cc7555dd276
Ivy Zhang <Zivy042003@gmail.com>
2021-01-16 11:52:22 -0500

    basic infrastructure for behavior

--- src/framework1/BotPolitician.java
+++ src/framework1/BotPolitician.java
@@ -4,14 +4,20 @@ import battlecode.common.*;
 
 public class BotPolitician extends Bot {
     public static void turn() throws GameActionException {
-        Team enemy = rc.getTeam().opponent();
-            int actionRadius = rc.getType().actionRadiusSquared;
-            RobotInfo[] attackable = rc.senseNearbyRobots(actionRadius, enemy);
-            if (attackable.length != 0 && rc.canEmpower(actionRadius)) {
-                rc.empower(actionRadius);
-                return;
-            }
-            if (Bot.tryMove(Bot.randomDirection()))
-                System.out.println("I moved!");
+        here = rc.getLocation();
+
+        int actionRadius = rc.getType().actionRadiusSquared;
+        double buff = rc.getEmpowerFactor(us, 0);
+
+        RobotInfo[] nearbyEnemies = rc.senseNearbyRobots(actionRadius, them);
+        RobotInfo[] nearbyAllies = rc.senseNearbyRobots(actionRadius, us);
+
+        int conviction = (int)(rc.getConviction() * buff);
+        if (nearbyEnemies.length != 0 && rc.canEmpower(actionRadius)) {
+            rc.empower(actionRadius);
+            return;
+        }
+        if (Bot.tryMove(Bot.randomDirection()))
+            System.out.println("I moved!");
     }
 }
