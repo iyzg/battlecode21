8fc58549b0fc15535db8fad87b5fc939a1d893d5
Ivy Zhang <Zivy042003@gmail.com>
2021-01-16 10:33:16 -0500

    Slanderer compressed lattice

--- src/framework1/BotSlanderer.java
+++ src/framework1/BotSlanderer.java
@@ -4,6 +4,8 @@ import battlecode.common.*;
 
 public class BotSlanderer extends Bot {
     public static void turn() throws GameActionException {
+        here = rc.getLocation();
+        if ((here.x + here.y) % 2 == 0) return;
         if (Bot.tryMove(Bot.randomDirection())) return;
     }
 }
